49694c914431fc85ffefa1f32e2edae30ea93dc0
François REMY <fremycompany_pub@yahoo.fr>
2015-10-20 14:27:28 -0700

    Working on pull request comments

--- src/cssUsage.js
+++ src/cssUsage.js
@@ -2,26 +2,43 @@
 // Guards execution against invalid conditions
 //
 void function() {
-	// Don't run in subframes for now
-	if (window.self !== window.top) throw new Error("CSSUsage: the script doesn't run in frames for now");
+    // Don't run in subframes for now
+    if (window.self !== window.top) throw new Error("CSSUsage: the script doesn't run in frames for now");
 }
 
 //
 // Prepare our global namespace
 //
 void function() {
-	window.CSSUsage = {};
-	window.CSSUsageResults = {
-		types: [
-			0,0,0,0,
-			0,0,0,0,
-			0,0,0,0,
-			0,0,0,0,
-		],
-		props: {
-			__proto__:null,//optimization
-		},
-	}
+    window.CSSUsage = {};
+	typesMap: 
+    window.CSSUsageResults = {
+		
+		// this will contains the usage stats of various at-rules and rules
+        types: [ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, ], /* 
+		types ~= {
+			"unknown":0,   //0
+			"style":0,     //1
+			"charset": 0,  //2
+			"import":0,    //3
+			"media":0,     //4
+			"font-face":0, //5
+			"page":0,      //6
+			"keyframes":0, //7 This is the @keyframe at rule
+			"keyframe":0,  //8 This is the individual 0%, or from/to
+			"reserved9":0, //9
+			"namespace":0, //10
+			"reserved11":0,//11
+			"supports":0,  //12
+			"reserved13":0,//13
+			"reserved14":0,//14
+			"viewport":0,  //15
+		}*/
+		
+		// this will contains the usage stats of various css properties and values
+        props: Object.create(null),
+		
+    }
 }();
 
 //
@@ -29,14 +46,14 @@ void function() {
 //
 void function() { "use strict";
 
-	CSSUsage.StyleWalker = {
-		ruleAnalyzers: [],
-		parseStylesheets: parseStylesheets,
-		parseInlineStyle: parseInlineStyle,
-	}
+    CSSUsage.StyleWalker = {
+        ruleAnalyzers: [],
+        parseStylesheets: parseStylesheets,
+        parseInlineStyle: parseInlineStyle,
+    }
 
     function parseStylesheets() {
-        var styleSheets = document.styleSheets;           
+        var styleSheets = document.styleSheets;
 
         // Loop through StyeSheets
         for (var ssIndex in styleSheets) {
@@ -45,9 +62,9 @@ void function() { "use strict";
                 parseCssRules(styleSheet.cssRules, styleSheet);
             }
             catch (e) {
-				console.log(e);
+                console.log(e);
                 if (e.description.indexOf("Access") != -1) {
-					styleSheetsToProcess--; // Since we can't parse this one we shouldn't depend on it to return results
+                    styleSheetsToProcess--; // Since we can't parse this one we shouldn't depend on it to return results
                 }
             }
         }
@@ -68,9 +85,9 @@ void function() { "use strict";
 
             // Loop through Rules
             for (var ruleIndex in cssRules) {
-                
+
                 var rule = cssRules[ruleIndex];
-                
+
                 // Until we can correlate animation usage
                 // to keyframes do not parse @keyframe rules
                 // TODO: https://github.com/gregwhitworth/css-usage/issues/3
@@ -89,16 +106,16 @@ void function() { "use strict";
                 rulesProcessed++;
 
             }
-			
-			// If we're done processing all of the CSS rules for
-			// this stylesheet
-			if (rulesProcessed == totalRules) {
-				styleSheetsProcessed++;             
-			}
-			
+
+            // If we're done processing all of the CSS rules for
+            // this stylesheet
+            if (rulesProcessed == totalRules) {
+                styleSheetsProcessed++;
+            }
+
         }
     }
-	
+
     /*
      * This creates a dom tree to walk over so that you can
      * get the inline styles and track those
@@ -120,15 +137,15 @@ void function() { "use strict";
             }
         }
     }
-	
-	/*
-	 *
-	 */
-	function runRuleAnalyzers(style, selector, type, isInline) {
-		CSSUsage.StyleWalker.ruleAnalyzers.forEach(function(runAnalyzer) {
-			runAnalyzer(style, selector, type, isInline);
-		});
-	}
+
+    /*
+     *
+     */
+    function runRuleAnalyzers(style, selector, type, isInline) {
+        CSSUsage.StyleWalker.ruleAnalyzers.forEach(function(runAnalyzer) {
+            runAnalyzer(style, selector, type, isInline);
+        });
+    }
 
 }();
 
@@ -136,32 +153,33 @@ void function() { "use strict";
 // helper to work with css values
 //
 void function() {
-	
-	CSSUsage.CSSValues = {
-		createValueArray: createValueArray,
-		parseValues: parseValues
-	};
-	
-	/*
+
+    CSSUsage.CSSValues = {
+        createValueArray: createValueArray,
+        parseValues: parseValues,
+		normalizeValue: normalizeValue
+    };
+
+    /*
      * This takes a string value and will create
      * an array from it.
      */
     function createValueArray(value) {
-        var values = new Array();          
+        var values = new Array();
 
         value = normalizeValue(value);
 
         // Parse values like: width 1s height 5s time 2s
-        if (Array.isArray(value)) {    
-                            
-            value.forEach(function(val) {          
+        if (Array.isArray(value)) {
+
+            value.forEach(function(val) {
                 // We need to trim again as fonts at times will
                 // be Font, Font2, Font3 and so we need to trim
                 // the ones next to the commas
-                val = trim(val);
+                val = val.trim();
                 values.push(val);
             });
-			
+
         }
         // Put the single value into an array so we get all values
         else {
@@ -170,7 +188,7 @@ void function() {
 
         return values;
     }
-	
+
     /*
      * This will take a string value and reduce it down
      * to only the aspects of the value we wish to keep
@@ -189,42 +207,42 @@ void function() {
              return value.replace(/(\d+)|(\-\d+)|(\.)/g, ""); // Remove any digits eg: 55px -> px
          }
     }
-	
-	//-----------------------------------------------------------------------------
-	
-	// This will normalize the values so that
+
+    //-----------------------------------------------------------------------------
+
+    // This will normalize the values so that
     // we only keep the unique values
     function normalizeValue(value) {
-		
+
         // Trim value on the edges
-        value = trim(value);
-        
+        value = value.trim();
+
         // Remove (
         if (value.indexOf("(") != -1) {
             value = value.replace(/\(([^\)]+)\)/g, "");
-        }       
-        
+        }
+
         // Remove varous quotes
         if (value.indexOf("'") != -1 || value.indexOf("‘") != -1 || value.indexOf('"')) {
             value = value.replace(/('|‘|’|")/g, "");
         }
-        
+
         value = value.toLowerCase();
-        
-        // We need to check if there are commas or spaces and 
-        // split on those so that we can keep track of each  
-        if (value.indexOf(" ") != -1 || value.indexOf(",") != - 1) {                       
+
+        // We need to check if there are commas or spaces and
+        // split on those so that we can keep track of each
+        if (value.indexOf(" ") != -1 || value.indexOf(",") != - 1) {
             if(value.indexOf(",") != -1) {
                 value = value.split(",");
             }
             else {
                 value = value.split(" ");
             }
-        }  
-        
-        return value;    
+        }
+
+        return value;
     }
-	
+
     /*
      * So that we don't end up with a ton of color
      * values, this will determine if the color is a
@@ -233,15 +251,11 @@ void function() {
     function isKeywordColor(color) {
         // Keyword colors from the W3C specs
         var colors = ["aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgreen", "lightgrey", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "navyblue", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"];
-    
+
         if (colors.indexOf(color) != -1) {
             return true;
         }
         return false;
-    }
-	
-	function trim(str) {
-        return str.replace(/(^\s+)|(\s+$)/g, "");
     }
 
 }();
@@ -250,18 +264,19 @@ void function() {
 // computes various css stats
 //
 void function() {
-	
-	CSSUsage.PropertyValuesAnalyzer = parseStyle;
-	
-	// This will loop over the styles declarations
-	function parseStyle(style, selector, type, isInline) {
+
+    CSSUsage.PropertyValuesAnalyzer = parseStyle;
+	CSSUsage.PropertyValuesAnalyzer.cleanSelectorText = cleanSelectorText;
+
+    // This will loop over the styles declarations
+    function parseStyle(style, selector, type, isInline) {
         for (var key in style) {
 
             var normalizedKey = normalizeKey(key);
-            
+
             // Only keep styles that were declared by the author
             if (style.cssText.indexOf(normalizedKey) == -1) continue;
-            
+
             // Chrome puts integer keys in for used props and we don't want to parse those
             if (isInteger(key)) continue;
 
@@ -291,28 +306,28 @@ void function() {
 
             var values = CSSUsage.CSSValues.createValueArray(styleValue);
 
-			// instanciate or fetch the property metadata
+            // instanciate or fetch the property metadata
             var propObject = CSSUsageResults.props[normalizedKey];
-            if (!propObject) { 
-				propObject = CSSUsageResults.props[normalizedKey] = { 
-					name: normalizedKey, 
-					count: 0, 
-					type: type, 
-					values: [] 
-				}; 
-			}
-			
-			
-			// increment the amount of affected elements
+            if (!propObject) {
+                propObject = CSSUsageResults.props[normalizedKey] = {
+                    name: normalizedKey,
+                    count: 0,
+                    type: type,
+                    values: []
+                };
+            }
+
+
+            // increment the amount of affected elements
             propObject.count += count;
 
             values.forEach(function (value) {
                 value = CSSUsage.CSSValues.parseValues(value);
-                
+
                 if (value === " " || value === "") {
                     return;
                 }
-                
+
                 var valExists = valueExists(normalizedKey, value);
                 if (!valExists) {
                     propObject.values.push({ name: value, count: count });
@@ -327,10 +342,10 @@ void function() {
             });
         }
     }
-	
-	//-------------------------------------------------------------------------
-	
-	/*
+
+    //-------------------------------------------------------------------------
+
+    /*
      * The document.styleSheets lists all of the
      * styles in camel case (e.g. "transitionDelay") but
      * we store them in our props as "transition-delay" so we need
@@ -339,9 +354,9 @@ void function() {
     function normalizeKey(key) {
         key = key.replace(/([a-z])([A-Z])/g, "$1-$2");
         key = key.toLowerCase();
-		switch(key) { case 'stylefloat': case 'cssfloat': return 'float'; default: return key; }
+        switch(key) { case 'stylefloat': case 'cssfloat': return 'float'; default: return key; }
     }
-	
+
     /*
      * This will scan the CSS Props object array
      * and determine if the object exists
@@ -355,14 +370,14 @@ void function() {
      * to determine if the value exists
      */
     function valueExists(propertyName, valueName) {
-		var pvalues = CSSUsageResults.props[propertyName].values;
-		for (var vIndex in pvalues) {
-			if (pvalues[vIndex].name == valueName) {
-				return true;
-			}
+        var pvalues = CSSUsageResults.props[propertyName].values;
+        for (var vIndex in pvalues) {
+            if (pvalues[vIndex].name == valueName) {
+                return true;
+            }
         }
         return false;
-    }        
+    }
 
     /*
      * If you try to do querySelectorAll on pseudo selectors
@@ -386,38 +401,37 @@ void function() {
 // This is where we decide what to run, and when
 //
 void function() {
-	
-	if(document.readyState !== 'complete') { 
-		document.addEventListener("DOMContentLoaded", onready);
-	} else {
-		onready();
-	}
-	
-	function onready() {
-		debugger;
-		
-		// Keep track of duration
-		var startTime = new Date().getTime();
-		
-		// register tools
-		CSSUsage.StyleWalker.ruleAnalyzers.push(CSSUsage.PropertyValuesAnalyzer);
-		
-		// perform analysis
+
+    if(document.readyState !== 'complete') {
+        document.addEventListener("DOMContentLoaded", onready);
+    } else {
+        onready();
+    }
+
+    function onready() {
+        debugger;
+
+        // Keep track of duration
+        var startTime = performance.now();
+
+        // register tools
+        CSSUsage.StyleWalker.ruleAnalyzers.push(CSSUsage.PropertyValuesAnalyzer);
+
+        // perform analysis
         CSSUsage.StyleWalker.parseStylesheets();
         CSSUsage.StyleWalker.parseInlineStyle();
-        
+
         // Update duration
-        CSSUsageResults.duration = new Date().getTime() - startTime;
-        
+        CSSUsageResults.duration = performance.now() - startTime;
+
         // DO SOMETHING WITH THE CSS OBJECT HERE
         console.log(CSSUsageResults);
-		
-		// Convert it to a more efficient format
-		Object.values = Object.values || function(o) { return Object.keys(o).map(function(k) { return o[k]; }) };
-		CSSUsageResults.types = Object.values(CSSUsageResults.types);
-		CSSUsageResults.props = Object.values(CSSUsageResults.props);	
-    
-	}
-	
-	
+
+        // Convert it to a more efficient format
+        var getValuesOf = Object.values || function(o) { return Object.keys(o).map(function(k) { return o[k]; }) };
+        CSSUsageResults.props = getValuesOf(CSSUsageResults.props);
+
+    }
+
+
 }();
