142cb01677f76f6945db3dcd5521b0ecabc4ab1d
François REMY <fremycompany_pub@yahoo.fr>
2016-03-02 16:05:21 -0800

    Fixed: gzip on websites using "window.define(...)"
    Signed-off-by: François REMY <fremycompany_pub@yahoo.fr>

--- src/cssUsageResults.js
+++ src/cssUsageResults.js
@@ -43,6 +43,11 @@ void function() {
 		// we tried...
 	}
 	
+	/* now, let's help browser to optimize this simple function */
+	for(var i=100; i--;) {
+		if(window.pako) { pako.string2buf(" "); }
+	}
+	
 }();
 
 window.onCSSUsageResults = function onCSSUsageResults(CSSUsageResults) {
@@ -76,14 +81,47 @@ window.onCSSUsageResults = function onCSSUsageResults(CSSUsageResults) {
 		}
 	}
 	
+	function uint6ToB64(nUint6) {
+		return (
+			nUint6 < 26 ? nUint6 + 65
+			: nUint6 < 52 ? nUint6 + 71
+			: nUint6 < 62 ? nUint6 - 4
+			: nUint6 === 62 ? 43
+			: nUint6 === 63 ? 47
+			: 65
+		);
+	}
+
+	function base64EncArr(aBytes) {
+
+	  var nMod3 = 2, sB64Enc = "";
+
+	  for (var nLen = aBytes.length, nUint24 = 0, nIdx = 0; nIdx < nLen; nIdx++) {
+		nMod3 = nIdx % 3;
+		//if (nIdx > 0 && (nIdx * 4 / 3) % 76 === 0) { sB64Enc += "\r\n"; }
+		nUint24 |= aBytes[nIdx] << (16 >>> nMod3 & 24);
+		if (nMod3 === 2 || aBytes.length - nIdx === 1) {
+		  sB64Enc += String.fromCharCode(uint6ToB64(nUint24 >>> 18 & 63), uint6ToB64(nUint24 >>> 12 & 63), uint6ToB64(nUint24 >>> 6 & 63), uint6ToB64(nUint24 & 63));
+		  nUint24 = 0;
+		}
+	  }
+
+	  return sB64Enc.substr(0, sB64Enc.length - 2 + nMod3) + (nMod3 === 2 ? '' : nMod3 === 1 ? '=' : '==');
+
+	}
+	
 	// Convert into one signle tsv file
 	var tsvString = INSTRUMENTATION_RESULTS_TSV.map((row) => (row.join('\t'))).join('\n');
 	if(window.debugCSSUsage) console.log(tsvString);
 	
+	// Compress the data if needed
+	var needsCompression = INSTRUMENTATION_RESULTS.UA=="EDGE";
+	if(needsCompression) { tsvString = "\r\n"+base64EncArr(pako.deflate(tsvString))+"\r\n"; }
+	
 	// Add it to the document dom
 	var output = document.createElement('script');
 	output.id = "css-usage-tsv-results";
-	output.textContent = pako.deflate(tsvString, { to: 'string' });
+	output.textContent = tsvString;
     output.type = 'text/plain';
     document.querySelector('head').appendChild(output);
 
