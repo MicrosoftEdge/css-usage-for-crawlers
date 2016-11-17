6bfaf37b39c55442cc206b87b7a832ac0f5d0d9b
Greg Whitworth <gwhit@microsoft.com>
2016-11-16 18:29:14 -0800

    modified grunt order, and fixed missing func names due to minimization that caused recipes to disappear

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,36 +7,13 @@ module.exports = function(grunt) {
         src: {
             src: [
               'src/cssUsageResults.js',
-              'css-usage/cssUsage.min.js',
+              'css-usage/cssUsage.src.js',
             ],
             dest: 'cssUsage.src.js'
         }
-    },
-    babel: {
-        options: {
-            compact: true,
-            comments: false,
-            sourceMap: false,
-            presets: ['es2015']
-        },
-        dist: {
-            files: {
-                'cssUsage.min.js': ['cssUsage.src.js']
-            }
-        }
-    },
-    uglify: {
-      options: { maxLineLen: 0 },
-      dist: {
-        files: {
-          'cssUsage.min.js': ['cssUsage.min.js']
-        }
-      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-babel');
-  grunt.registerTask('default', ['concat:src','babel:dist','uglify:dist']);
+  grunt.registerTask('default', ['concat:src']);
 };
